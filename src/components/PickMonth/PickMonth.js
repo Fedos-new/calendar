df275b8a5c15a8421a98cdc581d77bf911495662
Fedos-new <fedluky@gmail.com>
2021-01-08 21:55:17 +0700

    render PickMonth

--- src/components/PickMonth/PickMonth.js
+++ src/components/PickMonth/PickMonth.js
@@ -1,34 +1,97 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import style from './PickMonth.module.css'
 
+const ref = React.createRef();
+
+export const PickMonth = (props) => {
+
+    const [selectedDate, setSelectedDate] = useState(null)
+    const [date, setDate] = useState(props.defaultData.date)
+    const yearSelect = useRef(date)
+    const monthSelect = useRef(date)
+
+    const {years, monthNames, weekDayNames} = props.defaultData
+
+    const handlePrevMonthButtonClick = () => {
+        const prevDate = new Date(date.getFullYear(), date.getMonth() - 1)
+        setDate(prevDate)
+        console.log(prevDate)
+    }
+    const handleNextMonthButtonClick = () => {
+        const nextDate = new Date(date.getFullYear(), date.getMonth() + 1)
+        setDate(nextDate)
+        console.log(nextDate)
+
+    }
 
 
-export const PickMonth = () => {
+    const handleSelectChange = () => {
+        debugger
+
+        // const date = new Date(yearSelect.value, monthSelect.value)
+        // setSelectedDate(date)
+        // console.log(date);
+    }
+
+    const handleDayClick = (date) => {
+        setSelectedDate(date)
+        props.defaultData.onChange(date)
+    }
 
 
     return (
         <div className={style.wrap}>
             <header>
-                <button className={style.btn}>{'<'}</button>
-                <select className={style.select}></select>
+                <button className={style.btn}
+                        onClick={handlePrevMonthButtonClick}>
+                    {'<'}
+                </button>
+                <select className={style.select}
+                        onChange={handleSelectChange}
+                        ref={ref}
+                >
+                    {monthNames.map((name, index) =>
+                        <option key={name} value={index}>{name}</option>
+                    )}
+                </select>
 
-                <select className={style.select}></select>
-                <button className={style.btn}>{'>'}</button>
+                <select className={style.select}
+                        onChange={handleSelectChange}
+                        ref={ref}
+                >
+                    {years.map((name, index) =>
+                        <option key={name} value={index}>{name}</option>
+                    )}
+                </select>
+                <button className={style.btn}
+                        onClick={handleNextMonthButtonClick}>
+                    {'>'}
+                </button>
             </header>
-            <table>
+
+            <table className={style.tableData}>
                 <thead>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                <tr>
+                    {weekDayNames.map((day) =>
+                        <th key={day}>{day}</th>
+                    )}
+                </tr>
                 </thead>
                 <tbody>
-
+                {props.defaultDMonthData.map((week, index) =>
+                    <tr key={index} className={style.week}>
+                        {week.map((date, index) =>
+                            <td key={index}>{
+                                date
+                                    ? <th
+                                        key={index}
+                                        className={style.day}
+                                        onClick={() => handleDayClick(date)}>{date.getDate()} </th>
+                                    : <th/>
+                            }</td>
+                        )}
+                    </tr>
+                )}
                 </tbody>
             </table>
         </div>
--- src/components/PickMonth/PickMonthContainer.js
+++ src/components/PickMonth/PickMonthContainer.js
@@ -3,9 +3,27 @@ import {PickMonth} from "./PickMonth";
 
 
 export const PickMonthContainer = () => {
+    const defaultData = {
+        date: new Date(),
+        years: [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+        monthNames: ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь", "Ноябрь","Декабрь"],
+        weekDayNames: ["ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"],
+        onChange: Function.prototype
+    }
+    const  defaultDMonthData = [
+        [undefined, undefined, new Date() , new Date(),new Date(), new Date(), new Date()],
+        [new Date(), new Date(), new Date(), new Date(),new Date(), new Date(), new Date()],
+        [new Date(), new Date(), new Date(), new Date(),new Date(), new Date(), new Date()],
+        [new Date(), new Date(), new Date(), new Date(),new Date(), new Date(), new Date()],
+        [new Date(), new Date(), new Date(), new Date(),undefined, undefined, undefined]
+    ]
+
+
     return (
         <div>
-            <PickMonth/>
+            <PickMonth
+                defaultData={defaultData}
+                defaultDMonthData={defaultDMonthData}/>
         </div>
     )
 }
